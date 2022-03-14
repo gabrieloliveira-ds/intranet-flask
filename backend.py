3edf794f265c794ba7205049efe03c83d4d9df11
gabriel <dsgabrieloliveira@gmail.com>
2022-03-14 14:58:45 -0300

    corrigindo flask e adicionando template

---
+++ backend.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, render_template, request, redirect, session, flash, url_for
+app = Flask(__name__, template_folder='template')
+
+
+@app.route('/')
+def index():
+    return render_template('template.html',title= 'Atenas Intranet') 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
