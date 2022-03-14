104d49319635b77cd679109a520ed9a0bb0469fa
gabriel <dsgabrieloliveira@gmail.com>
2022-03-14 16:40:30 -0300

    navegação entre as paginas

--- backend.py
+++ backend.py
@@ -1,11 +1,27 @@
+from turtle import title
 from flask import Flask, redirect, render_template, request, redirect, session, flash, url_for
+
+#configurando o app
 app = Flask(__name__, template_folder='template')
 
-
+#paginas
 @app.route('/')
 def index():
-    return render_template('index.html',title= 'Atenas Intranet') 
+    return render_template('index.html',title= 'Atenas Intranet|HOME') 
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html',title= 'Atenas Intranet|CADASTRO')
 
+@app.route('/cadastro-funcionario')
+def cadastro_funcionario():
+    return render_template('cadastro-funcionario.html',title='Atenas Intranet|CADASTRO')
+
+@app.route('/cadastro-cliente')
+def cadastro_cliente():
+    return render_template('cadastro-cliente.html',title='Atenas Intranet|CADASTRO')
+#rotas
+
+#inicialização 
 if __name__ == "__main__":
     app.run(debug=True)
